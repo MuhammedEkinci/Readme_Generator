7ada91e66a706f5b21b53e7a9d1b2c1b6ea4f683
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-09-19 11:20:41 -0400

    added questions

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
-// array of questions for user
+const inquirer = require("inquirer");
+var fs = require("fs");
+
+// Questions
 const questions = [
+    {
+        type: "input",
+        name: "github",
+        message: "What's your username on github",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: `What kind of license does your project have`,
+    },
+    {
+        type: "input",
+        name: "email",
+        message: `What's your email address?`,
+    },
+    {
+        type: "input",
+        name: "title",
+        message: `What is your project's name?`,
+    },
+    {
+        type: "input",
+        name: "descriptions",
+        message: `Can you tell me about your project?`,
+    },
 
+    {
+        type: "input",
+        message: `Give instructions on how to install.`,
+        name: "install",
+    },
+
+    {
+        type: "input",
+        message: `What makes a program run and allows memory to be ______?`,
+        name: "usage",
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: `Who's a contributor for this project?`,
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "what are we running",
+    },
 ];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
